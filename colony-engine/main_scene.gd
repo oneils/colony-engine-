07108284133dd0d56707c11cc88d2ac2a5a120c7
oneils <lex.nox@gmail.com>
2026-05-17 22:09:33 +0200

    fix http request leaks and error handling
    - moved HTTPRequest creation and signal connection to _ready, was
    recreating node and reconnecting signal on every fetch
      - replaced fixed timer with request chaining to avoid ERR_IN_USE
      - added result/response_code check before parsing json to prevent
    crash when server is down

--- colony-engine/main_scene.gd
+++ colony-engine/main_scene.gd
@@ -2,13 +2,14 @@ extends Node2D
 
 var current_positions = {}
 var target_positions = {}
+var http_request: HTTPRequest
 
 func _ready() -> void:
-	var timer = Timer.new()
-	add_child(timer)
-	timer.wait_time = 0.1
-	timer.timeout.connect(_fetch_state)
-	timer.start()
+	http_request = HTTPRequest.new()
+	add_child(http_request)
+	http_request.request_completed.connect(_on_request_completed)
+
+	_fetch_state()
 
 func _process(delta: float) -> void:
 	var changed = false
@@ -21,18 +22,20 @@ func _process(delta: float) -> void:
 		queue_redraw()
 
 func _fetch_state():
-	var http_request = HTTPRequest.new()
-	add_child(http_request)
-	http_request.request_completed.connect(_on_request_completed)
 	var error = http_request.request("http://localhost:8080/npcs/state")
 	if error != OK:
 		push_error("Can't perform HTTP request")
 
 func _on_request_completed(result, response_code, headers, body):
-	var json = JSON.parse_string(body.get_string_from_utf8())
-	for npc in json["npcs"]:
-		var id = str(npc["id"])
-		target_positions[id] = Vector2(npc["position"]["x"], npc["position"]["y"])
+	if result == HTTPRequest.RESULT_SUCCESS and response_code == 200:
+		var json = JSON.parse_string(body.get_string_from_utf8())
+		if json and json.has("npcs"):
+			for npc in json["npcs"]:
+				var id = str(npc["id"])
+				target_positions[id] = Vector2(npc["position"]["x"], npc["position"]["y"])
+
+	await get_tree().create_timer(0.1).timeout
+	_fetch_state()
 
 func _draw():
 	for id in current_positions:
